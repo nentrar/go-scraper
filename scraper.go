6eb1f3d0e34ea85090e88b4c917ee9edef3fd85b
Piotr Semeniuk <TI5BGAZ7N(#8)#D>
2024-10-01 12:08:25 +0200

    Add main functions

--- scraper.go
+++ scraper.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	//"fmt"
+
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	// logic
 
-	fmt.Println("Hello, world!")
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong: ", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Page visited: ", r.Request.URL)
+	})
+
+	c.OnHTML("a", func(e *colly.HTMLElement) {
+		fmt.Println("%v", e.Attr("href"))
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(r.Request.URL, " scraped!")
+	})
+
+	c.Visit("https://www.scrapingcourse.com/ecommerce")
 }
