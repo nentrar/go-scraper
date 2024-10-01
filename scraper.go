f13baf11c7b3a4e0be400fd82ea27d654e94e09d
Piotr Semeniuk <TI5BGAZ7N(#8)#D>
2024-10-01 12:50:58 +0200

    Add CSV write on scraped

--- scraper.go
+++ scraper.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
@@ -44,6 +47,37 @@ func main() {
 	})
 
 	c.OnScraped(func(r *colly.Response) {
+
+		file, err := os.Create("products.csv")
+		if err != nil {
+			log.Fatalln("Failed to create output CSV file", err)
+		}
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+
+		headers := []string{
+			"Url",
+			"Image",
+			"Name",
+			"Price",
+		}
+
+		writer.Write(headers)
+
+		for _, product := range products {
+			record := []string{
+				product.Url,
+				product.Image,
+				product.Name,
+				product.Price,
+			}
+
+			writer.Write(record)
+		}
+
+		defer writer.Flush()
+
 		fmt.Println(r.Request.URL, " scraped!")
 	})
 
